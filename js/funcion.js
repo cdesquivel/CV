d04021ca309fe67839106a0d2a129980c01bf617
Cesar Daniel <cdesquivel97@gmail.com>
2023-09-24 22:43:09 -0300

    CV proyecto

--- js/funcion.js
+++ js/funcion.js
@@ -1,4 +1,14 @@
-document.getElementById("boton").onclick = function () {
+document.getElementById('boton').onclick = function () {
     console.log("Capturamos el evento click");
-    document.getElementById("demo").innerHTML = "Roberto Jiménez";
+    document.getElementById('demo').innerHTML = "(475)682-6334"
+}
+
+document.getElementById('boton2').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById('demo2').innerHTML = "Master en Gastronomía"
+}
+
+document.getElementById('boton3').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById('demo3').innerHTML = "Manipulación de los alimentos"
 }
